fb776d97e0f608dd888cb0c41f55a093bcd6b7cb
loft-vad <57989395+loft-vad@users.noreply.github.com>
2024-03-26 20:26:37 +0100

    Homework 3 additional work

--- product-service/handlers.js
+++ product-service/handlers.js
@@ -8,23 +8,6 @@ const defaultHeaders = {
   "Access-Control-Allow-Headers": "*"
 };
 
-// module.exports.getProductsList = async (event) => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(
-//       {
-//         message: 'Go Serverless v1.0! Your function executed successfully!',
-//         input: event,
-//       },
-//       null,
-//       2
-//     ),
-//   };
-
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
-
 export const getProductsList = async (event) => {
   const products = await productsList();
   products.headers = defaultHeaders;
